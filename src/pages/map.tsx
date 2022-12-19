91f8874f65abfdd80b23fdafe988b68e5e53ade0
Isaiah Abiodun <isaiah.abiodun@outlook.com>
2022-12-19 15:42:26 -0500

    refactor: google map setup contd.

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -9,15 +9,30 @@ import { SEO } from "@portfolio-components/seo";
 import { Spinner } from "flowbite-react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+
 const Map: React.FC = ({ transitionStatus, entry }: any) => {
   function MyMapComponent({ center, zoom }: { center: google.maps.LatLngLiteral; zoom: number }) {
     const ref = React.createRef<HTMLDivElement>();
 
+    // function initMap(): void {
+    //   new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    //     center: { lat: -34.397, lng: 150.644 },
+    //     zoom: 8,
+    //   });
+    // }
+    // window.initMap = initMap;
     React.useEffect(() => {
       new window.google.maps.Map(ref.current as HTMLDivElement, {
         center,
         zoom,
       });
+
+      // initMap();
     }, []);
 
     return <div ref={ref} id="map" />;
@@ -33,7 +48,7 @@ const Map: React.FC = ({ transitionStatus, entry }: any) => {
   const zoom = 4;
 
   return (
-    <Wrapper apiKey={"AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg"} render={render}>
+    <Wrapper apiKey={"AIzaSyBtninS1JvXDdWUNcZspWURij-t9gcQKkE"} render={render}>
       <MyMapComponent center={center} zoom={zoom} />
     </Wrapper>
   );
